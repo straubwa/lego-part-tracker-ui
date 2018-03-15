8de0306ae5c052da399932fa1e9effef9b8ea9f8
Wayne Straub <waynestraub@gmail.com>
2018-03-14 23:40:46 -0400

    added base set list component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { SetListComponent } from './sets/set-list.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SetListComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/sets/set-list.component.ts
@@ -0,0 +1,9 @@
+import {Component} from '@angular/core';
+
+@Component({
+    selector: 'app-sets',
+    templateUrl: './set-list.component.html'
+})
+export class SetListComponent {
+    pageTitle: string = 'List of Lego Sets';
+}
