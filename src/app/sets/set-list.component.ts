7b06206633c0518815a7f74bc60617fea17ae730
Wayne Straub <waynestraub@gmail.com>
2018-03-17 17:22:56 -0400

    added some example lego set data, and displayed it in the set list grid

--- src/app/sets/set-list.component.ts
+++ src/app/sets/set-list.component.ts
@@ -6,4 +6,43 @@ import {Component} from '@angular/core';
 })
 export class SetListComponent {
     pageTitle: string = 'List of Lego Sets';
+    imageWidth: number = 200;
+    imageMargin: number = 2;
+    sets: any[] = [
+        {
+            "set_num": "70612-1",
+            "name": "Green Ninja Mech Dragon",
+            "theme_id": 616,
+            "theme": "Ninjago > Ninjago The Movie",
+            "set_img_url": "https://m.rebrickable.com/media/sets/70612-1.jpg"
+        },
+        {
+            "set_num": "70614-1",
+            "name": "Lightning Jet",
+            "theme_id": 616,
+            "theme": "Ninjago > Ninjago The Movie",
+            "set_img_url": "https://m.rebrickable.com/media/sets/70614-1.jpg"
+        },
+        {
+            "set_num": "70608-1",
+            "name": "Master Falls",
+            "theme_id": 616,
+            "theme": "Ninjago > Ninjago The Movie",
+            "set_img_url": "https://m.rebrickable.com/media/sets/70608-1.jpg"
+        },
+        {
+            "set_num": "70611-1",
+            "name": "Water Strider",
+            "theme_id": 616,
+            "theme": "Ninjago > Ninjago The Movie",
+            "set_img_url": "https://m.rebrickable.com/media/sets/70611-1.jpg"
+        },
+        {
+            "set_num": "70656-1",
+            "name": "garmadon, Garmadon, GARMADON!",
+            "theme_id": 435,
+            "theme": "Ninjago",
+            "set_img_url": "https://m.rebrickable.com/media/sets/70656-1.jpg"
+        }
+    ];
 }
