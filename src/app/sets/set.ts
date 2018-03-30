93f323ef18b3caea33bcc7da2f3e86bd282ede66
Wayne Straub <waynestraub@gmail.com>
2018-03-29 22:21:49 -0400

    changed ISet to match new service names

--- src/app/sets/set.ts
+++ src/app/sets/set.ts
@@ -1,7 +1,7 @@
 export interface ISet {
-    "set_num": string;
+    "setNumber": string;
     "name": string;
-    "theme_id": number;
+    "themeId": number;
     "theme": string;
-    "set_img_url": string;
+    "setImageUrl": string;
 }
